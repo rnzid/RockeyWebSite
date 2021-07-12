76f51ae80738ccd3b2f6c41e27fe2c587a7e66aa
rnzid <ranjitdangol13@gmail.com>
2021-07-12 16:55:08 +0545

    category Delete function completed

--- Rockys/Controllers/CategoryController.cs
+++ Rockys/Controllers/CategoryController.cs
@@ -75,5 +75,38 @@ namespace Rockys.Controllers
             return View(obj);
 
         }
+
+
+        //Get-Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //Post-Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
     }
 }
