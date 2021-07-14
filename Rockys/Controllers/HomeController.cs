e3ad69833786db52a2aa02692f42965fb8e358f3
rnzid <ranjitdangol13@gmail.com>
2021-07-14 15:54:40 +0545

    Product Details Page Added

--- Rockys/Controllers/HomeController.cs
+++ Rockys/Controllers/HomeController.cs
@@ -33,6 +33,17 @@ namespace Rockys.Controllers
             return View(homeVM);
         }
 
+        public IActionResult Details(int id)
+        {
+            DetailsVM DetailsVM = new DetailsVM()
+            {
+                Product = _db.Product.Include(u => u.Category).Include(u => u.ApplicationType)
+                .Where(u => u.Id == id).FirstOrDefault(),
+                ExistsInCart = false
+            };
+            return View(DetailsVM);
+        }
+
         public IActionResult Privacy()
         {
             return View();
