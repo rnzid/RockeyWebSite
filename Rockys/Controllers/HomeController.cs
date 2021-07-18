8cb5186148b6eb2c917f576911029fc7981e3676
rnzid <ranjitdangol13@gmail.com>
2021-07-18 15:47:41 +0545

    Producted added to cart counted

--- Rockys/Controllers/HomeController.cs
+++ Rockys/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Rockys.Data;
 using Rockys.Models;
 using Rockys.Models.ViewModels;
+using Rockys.Utility;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -42,6 +44,20 @@ namespace Rockys.Controllers
                 ExistsInCart = false
             };
             return View(DetailsVM);
+        }
+
+        [HttpPost,ActionName("Details")]
+        public IActionResult DetailsPost(int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+            shoppingCartList.Add(new ShoppingCart { ProductId = id });
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
