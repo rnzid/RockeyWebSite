880145b03aecf317f2ac72361ef3ca55efbbc4e7
rnzid <ranjitdangol13@gmail.com>
2021-07-26 12:59:39 +0545

    UI updates on create edit delete all

--- Rockys_DataAccess/Migrations/20210725094518_test1.Designer.cs
+++ Rockys_DataAccess/Migrations/20210725094518_test1.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Rockys_DataAccess;
 
 namespace Rockys_DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210725094518_test1")]
+    partial class test1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
